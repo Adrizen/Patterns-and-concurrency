c815ee2a13129083390de7b00d96e0c0a7fda195
Andy <Andy@DESKTOP-L2LNT2S>
2021-08-26 20:41:00 -0300

    prueba con beep

--- src/test.java
+++ src/test.java
@@ -1,5 +1,11 @@
+import java.util.concurrent.ScheduledExecutorService;
+
 public class test {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hola mundo!");
+        Beep beep = new Beep();
+        beep.beepForAnHour();
     }
+
 }
