e33e2b461de784ef1f366cf6554715bf52343f12
torrix <fabiotorrico@hotmail.com>
2021-08-30 00:28:43 -0300

    agrego el modulo que levanta el .txt

--- src/Usuario.java
+++ src/Usuario.java
@@ -10,6 +10,14 @@ public class Usuario implements Runnable {
         this.dni = d;
         this.interfazSIU = i;
     }
+  
+    public Usuario(String d, String a, String n, Token unt, InterfazSIU i) {
+        this.dni = d;
+        this.apellido=a;
+        this.nombre=n;
+        this.untoken=unt;
+        this.interfazSIU = i;
+    }
 
     public void run() {
         System.out.println("Soy " + this.dni + " y me quiero loguear.");
